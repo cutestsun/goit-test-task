fae0d5e7869f81a02712fc4472c9093edab30b9a
Illia Shulha <ilyxa2836@gmail.com>
2023-10-13 21:02:22 +0200

    adds favicon

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import SharedLayout from "../components/SharedLayout/SharedLayout";
+
 import Home from "../pages/Home";
 import Tweets from "../pages/Tweets";
 import { AppWrapper } from "./App.styled";
@@ -8,8 +8,7 @@ export default function App() {
   return (
     <AppWrapper>
       <Routes>
-        {/* <Route path="/" element={<SharedLayout />}>
-        </Route> */}
+        
         <Route path="/" element={<Home />}>
           <Route path="*" element={<Navigate to="/" />} />
         </Route>
